068be2bbdad7221eab8d408ca712ce0c6a6b8c0a
simpart <simpart>
2018-09-09 14:14:22 +0000

    mod init

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
  * @file   mofron-comp-prjtop/index.js
  * @author simpart
  */
-const mf = require('mofron');
+const mf     = require('mofron');
 const Button = require('mofron-comp-button');
-//const efCenter = require('mofron-effect-center');
+const Hriz   = require('mofron-layout-horizon');
 const Hrzpos = require('mofron-effect-hrzpos');
 const Vrtpos = require('mofron-effect-vrtpos');
-const Blur   = require('mofron-effect-blur');
 const Text   = require('mofron-comp-bgtext');
 const Image  = require('mofron-comp-image');
 
@@ -25,6 +24,7 @@ mf.comp.Prjtop = class extends mf.Component{
         try {
             super();
             this.name('Prjtop');
+            this.prmMap('text');
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -43,10 +43,7 @@ mf.comp.Prjtop = class extends mf.Component{
                 'position' : 'relative'
             });
             
-            /* image area */
-            this.target().addChild(this.getTarget(0));
-            
-            let cnt = new mf.Dom({
+            let cnt_ara = new mf.Dom({
                 tag       : 'div',
                 component : this,
                 style     : {
@@ -54,108 +51,38 @@ mf.comp.Prjtop = class extends mf.Component{
                     'top'      : '0px',
                     'width'    : '100%'
                 },
-                child : [
-                    this.getTarget(1), /* phrase area */
-                    this.getTarget(2), /* button area */
-                ]
+                //child : [ this.text() ]
             });
-            this.target().addChild(cnt);
-            this.target(cnt);
+            this.child([this.image()]);
+            this.target().addChild(cnt_ara);
+            this.target(cnt_ara);
             
-            this.height(
-                mf.func.convPx2Rem(window.innerHeight)  + 'px'
-            );
+            this.child([this.text(),this.button()]);
+            
+            this.height(window.innerHeight  + 'px');
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    getTarget (prm) {
-        try {
-            if (undefined === this.m_tgtarea) {
-                this.m_tgtarea = [
-                    /* image area */
-                    new mf.Dom({
-                        tag       : 'div',
-                        component : this,
-                        style     : { 'display': 'flex' }
-                    }),
-                    /* phrase area */
-                    new mf.Dom({
-                        tag       : 'div',
-                        component : this,
-                        style     : {
-                            'width'   : '100%',
-                            'display' : 'flex'
-                        }
-                    }),
-                    /* button area */
-                    new mf.Dom({
-                        tag       : 'div',
-                        component : this,
-                        style     : {
-                            'position' : 'absolute',
-                            'width'    : '100%',
-                            'bottom'   : '20%',
-                            'display'  : 'flex'
-                        }
-                    })
-                ];
-            }
-            if (undefined === this.m_tgtarea[prm]) {
-                throw new Error('invalid parameter');
-            }
-            return this.m_tgtarea[prm];
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    image (prm, blr) {
+    image (prm) {
         try {
             if (undefined === prm) {
                 /* getter */
-                return (undefined === this.m_image) ? null : this.m_image;
+                if (undefined === this.m_image) {
+                    this.image(new Image({ visible : false }));
+                }
+                return this.m_image;
             }
             /* setter */
-            if ('string' === typeof prm) {
-                prm = new Image(prm);
-            }
             if (true !== mf.func.isInclude(prm, 'Image')) {
                 throw new Error('invalid parameter');
             }
-            let eff = [ new Hrzpos('center') ];
-            if ('number' === typeof blr) {
-                eff.push(new Blur({ value : blr }));
+            prm.execOption({effect : [new Hrzpos('center')]});
+            if (undefined !== this.m_image) { 
+                this.updChild(this.m_image, prm);
             }
-            prm.execOption({
-                effect : eff
-            }); 
-            if (null === prm.width()) {
-                prm.execOption({
-                    width : '100%'
-                });
-            }
-            if (null === prm.height()) {
-                prm.execOption({
-                    height : this.height()
-                });
-            }
-            
-            this.switchTgt(
-                this.getTarget(0),
-                (swh_prm) => {
-                    try {
-                        swh_prm.addChild(prm);
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-                this
-            );
             this.m_image = prm;
         } catch (e) {
             console.error(e.stack);
@@ -163,13 +90,15 @@ mf.comp.Prjtop = class extends mf.Component{
         }
     }
     
-    phrase (prm) {
+    text (prm) {
         try {
             if (undefined === prm) {
                 /* getter */
-                return (undefined === this.m_phrase) ? null : this.m_phrase;
+                if (undefined === this.m_text) {
+                    this.text(new Text({ visible : false }));
+                }
+                return this.m_text;
             }
-            
             /* setter */
             if ('string' === typeof prm) {
                 prm = new Text(prm);
@@ -177,23 +106,15 @@ mf.comp.Prjtop = class extends mf.Component{
             if (true !== mf.func.isInclude(prm, 'Text')) {
                 throw new Error('invalid parameter');
             }
+            
             prm.execOption({
-                size   : 0.5,
-                effect : [ new Hrzpos('center'), new Vrtpos('center') ]
+                size   : '0.5rem',
+                effect : [ new Hrzpos('center'), new Vrtpos('center', '-15%') ]
             });
-            this.switchTgt(
-                this.getTarget(1),
-                (swh_prm) => {
-                    try {
-                        swh_prm.addChild(prm);
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-                this
-            );
-            this.m_phrase = prm;
+            if (undefined !== this.m_text) {
+                this.updChild(this.m_text, prm);
+            }
+            this.m_text = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -204,53 +125,33 @@ mf.comp.Prjtop = class extends mf.Component{
         try {
             if (undefined === prm) {
                 /* getter */
-                return (undefined === this.m_button) ? null : this.m_button;
+                if (undefined === this.m_button) {
+                    this.button(new Button({ visible : false }));
+                }
+                return this.m_button;
             }
             /* setter */
             if ('string' === typeof prm) {
-                prm = new Button({
-                          text   : prm,
-                          size   : new mf.Param(2, 0.4),
-                          effect : [ new Hrzpos('center') ]
-                      });
-            } else if (true !== mf.func.isInclude(prm, 'Button')) {
-                 throw new Error('invalid parameter');
+                prm = new Button(prm);
+            }
+            if (true !== mf.func.isInclude(prm, 'Button')) {
+                throw new Error('invalid parameter');
             }
             
-            let obj = this;
-            this.switchTgt(
-                this.getTarget(2),
-                (swh_prm) => {
-                    try {
-                        swh_prm.addChild(prm);
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-                this
-            );
+            prm.execOption({
+                size   : new mf.Param('2rem', '0.4rem'),
+                effect : [ new Hrzpos('center'), new Vrtpos('bottom', '40%') ]
+            });
+            
+            if (undefined !== this.m_button) {
+                this.updChild(this.m_button, prm);
+            }
             this.m_button = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
-    height (prm) {
-        try {
-            let ret = super.height(prm);
-            if ( (undefined === ret) && (null !== this.image()) ) {
-                this.getTarget(1).style({
-                    height : super.height()
-                });
-            }
-            return ret;
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
 }
-module.exports = mofron.comp.Prjtop;
+module.exports = mf.comp.Prjtop;
 /* end of file */
