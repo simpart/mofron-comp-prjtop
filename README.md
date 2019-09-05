194a4713c983348ae2ffd20420162c0d56c1a88f
simpart <simpart>
2019-09-05 13:46:04 +0900

    mod button

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # mofron-comp-prjtop
-project top component for mofron
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+project component for mofron
+
+it makes easy to build project top contents
+
+
+# Install
+```
+npm install mofron mofron-comp-prjtop
+```
+
+# Sample
+```html
+<require>
+    <tag module="mofron-comp-appbase">AppBase</tag>
+    <tag module="mofron-comp-prjtop">PrjTop</tag>
+</require>
+
+<AppBase>
+    <PrjTop text="Project Top Page">
+        <button>Get Started,"./"</button>
+    </PrjTop>
+</AppBase>
+```
+# Parameter
+
+|Simple<br>Param | Parameter Name | Type | Description |
+|:--------------:|:---------------|:-----|:------------|
+| | image | string | path to image |
+| | | string (size) | blur size |
+| ◯  | text | string | phrase text |
+| | button | mixed | mofron-comp-button: link component |
+| | | | string: button text |
+| | | string | link path |
+| | offset | string (size) | offset size |
+
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /**
- * @file   mofron-comp-prjtop/index.js
+ * @file mofron-comp-prjtop/index.js
+ * @brief project component for mofron
+ *        it makes easy to build project top contents
  * @author simpart
  */
 const mf      = require("mofron");
@@ -8,21 +10,21 @@ const Hriz    = require("mofron-layout-horizon");
 const Hrzpos  = require("mofron-effect-hrzpos");
 const Vrtpos  = require("mofron-effect-vrtpos");
 const SynWhei = require("mofron-effect-synwhei");
+const SynHei  = require("mofron-effect-synchei");
 const Blur    = require("mofron-effect-blur");
 const Link    = require("mofron-event-link");
 const Text    = require("mofron-comp-bgtext");
 const Image   = require("mofron-comp-image");
 
 let mod_name = "Prjtop";
-/**
- * @class mofron.comp.prjtop
- * @brief project top contents component for mofron
- */
 mf.comp.Prjtop = class extends mf.Component{
     /**
      * initialize component
      * 
-     * @param po paramter or option
+     * @param (mixed) text paramter
+     *                object: component option
+     * @pmap text
+     * @type private
      */
     constructor (po) {
         try {
@@ -39,12 +41,14 @@ mf.comp.Prjtop = class extends mf.Component{
     /**
      * initialize dom contents
      * 
+     * @type private
      */
     initDomConts () {
         try {
             super.initDomConts();
             this.target().style({ 'position' : 'relative' });
-            
+            let topdom = this.target();
+
             let cnt_ara = new mf.Component({ style: { 'width' : '100%' } });
             this.image().option({ child: [cnt_ara] });
             this.child([this.image()]);
@@ -53,24 +57,55 @@ mf.comp.Prjtop = class extends mf.Component{
             
             this.effect([new SynWhei({ tag: mod_name })]);
             this.style({ position:"absolute", top: "0px" });
+            
+	    this.height(window.innerHeight + "px");
+	    let prj = this;
+	    this.target().styleListener(
+	        'height',
+		(st1,st2) => {
+		    try {
+		        let hei = mf.func.getSize(st2.height);
+			topdom.style({ "height" : st2.height });
+		        prj.button().style({ "top" : hei.value() * 0.5 + "px" });
+			prj.image().height((hei.value() - 10) + "px");
+		    } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+	            }
+		}
+	    );
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    
+    /**
+     * image config
+     * 
+     * @param (string) path to image
+     * @param (string (size)) blur size
+     * @return (mofron-comp-image) image component
+     * @type parameter
+     */
     image (prm, blr) {
         try {
             if (true === mf.func.isInclude(prm, "Image")) {
                 prm.option({
                     width: "100%",
+		    style: {
+		        "position" : "relative",
+			"z-index"  : "400"
+		    },
                     effect: [
                         new Hrzpos('center'),
-                        new SynWhei({ tag: mod_name }),
                         new Blur({ tag: mod_name, value: (undefined === blr) ? '0rem' : blr })
                     ]
                 });
+		let o_img = this.m_inncmp["image"];
+		if ( (undefined !== o_img) && (0 < o_img.child().length) ) {
+		    prm.child(o_img.child());
+		}
             } else if ("string" === typeof prm) {
                 this.image().path(prm);
                 this.image().effect(["Blur", mod_name]).value(blr);
@@ -83,6 +118,13 @@ mf.comp.Prjtop = class extends mf.Component{
         }
     }
     
+    /**
+     * catchphrase
+     * 
+     * @param (string) phrase text
+     * @return (mofron-comp-text) text component
+     * @type parameter
+     */
     text (prm) {
         try {
             if ('string' === typeof prm) {
@@ -91,6 +133,10 @@ mf.comp.Prjtop = class extends mf.Component{
             } else if (true === mf.func.isComp(prm, 'Text')) {
                 prm.option({
                     size: '0.5rem',
+		    style: {
+		        "position" : "relative",
+			"z-index"  : "400"
+		    },
                     effect : [
                         new Hrzpos('center'),
                         new Vrtpos({ type: 'center', offset:'-15%', contsIndex: 1 })
@@ -104,6 +150,15 @@ mf.comp.Prjtop = class extends mf.Component{
         }
     }
     
+    /**
+     * get start button 
+     *
+     * @param (mixed) mofron-comp-button: link component
+     *                string: button text
+     * @param (string) link path
+     * @return (mofron-comp-button) button component
+     * @type parameter
+     */
     button (prm, pth) {
         try {
             if (undefined !== pth) {
@@ -111,8 +166,12 @@ mf.comp.Prjtop = class extends mf.Component{
             }
             if (true === mf.func.isComp(prm)) {
                 prm.option({
+		    style: {
+		        "position" : "relative",
+			"z-index"  : "500",
+		    },
                     size: ['2rem', '0.4rem'], visible: false,
-                    effect: [ new Hrzpos({ type: 'center', contsIndex: 2 }), new Vrtpos('bottom', '30%') ]
+                    effect: [ new Hrzpos({ type: 'center', contsIndex: 2 }) ]
                 });
             } else if ('string' === typeof prm) {
                 this.button().option({ text: prm, visible: true });
@@ -125,11 +184,16 @@ mf.comp.Prjtop = class extends mf.Component{
         }
     }
     
+    /**
+     * image offset
+     * 
+     * @param (string (size)) offset size
+     * @type parameter
+     */
     offset (prm) {
         try {
-            this.effect(["Synwhei", mod_name]).offset(prm);
-            this.image().effect(["Synwhei", mod_name]).offset(prm);
-        } catch (e) {
+	    return this.effect(["Synwhei", mod_name]).offset(prm);
+	} catch (e) {
             console.error(e.stack);
             throw e;
         } 
